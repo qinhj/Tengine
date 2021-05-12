dcfc69103da86ac2c8a2bf3bf3e7cf5c6426e8ed
qinhj <qinhj@lsec.cc.ac.cn>
2021-05-12 22:27:30 +0800

    [examples] yolov5s_uint8_: code review && refactor

--- examples/tm_yolov5s_uint8_.cpp
+++ examples/tm_yolov5s_uint8_.cpp
@@ -32,31 +32,12 @@
 #include <stdlib.h>
 /* std c++ includes */
 #include <vector>
-#include <cmath>    // for: exp
 /* imilab includes */
-#include "imilab/imi_utils_elog.h"
-#ifdef USE_OPENCV
-/* opencv includes */
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#else // !USE_OPENCV
-#include "imilab/imi_utils_types.hpp"
-#include "imilab/imi_imread.h"
-#endif // USE_OPENCV
 #include "imilab/imi_utils_coco.h"  // for: coco_class_names
-#include "imilab/imi_utils_sort.hpp"
+#include "imilab/imi_utils_object.hpp"
+#include "imilab/imi_utils_visual.hpp"
+#include "imilab/imi_utils_yolov5.hpp"
 #include "imilab/imi_utils_tm.h"    // for: imi_utils_tm_run_graph
-
-#ifdef USE_OPENCV
-using namespace cv;
-#endif // USE_OPENCV
-
-struct Object {
-    Rect2f rect;
-    int label;
-    float prob;
-};
 
 static int class_num = coco_class_num;
 static const char **class_names = coco_class_names;
@@ -64,126 +45,10 @@ static const char **class_names = coco_class_names;
 static const float prob_threshold = 0.4f; // 0.25f
 static const float nms_threshold = 0.40f; // 0.45f
 
-// allow none square letterbox, set default letterbox size
-static image lb = make_image(640, 640, 3);
-static const float cov[][3] = {
-    { 0, 0, 0 }, // mean
-    { 0.003921, 0.003921, 0.003921 } // scale
-};
-
-static inline float sigmoid(float x) {
-    return static_cast<float>(1.f / (1.f + exp(-x)));
-}
-
-// todo: optimize
-// @param:  feat[in]    anchor results: box.x, box.y, box.w, box.h, box.score, {cls.score} x cls.num
-static void generate_proposals(int stride, const float *feat, float prob_threshold, std::vector<Object>& objects) {
-    static float anchors[18] = { 10, 13, 16, 30, 33, 23, 30, 61, 62, 45, 59, 119, 116, 90, 156, 198, 373, 326 };
-
-    int cls_num = class_num; // 80
-    int out_num = 4 + 1 + cls_num;// rent, score, cls_num
-
-    int anchor_num = 3;
-    int anchor_group;
-    if (stride == 8)
-        anchor_group = 1;
-    if (stride == 16)
-        anchor_group = 2;
-    if (stride == 32)
-        anchor_group = 3;
-
-    int feat_w = lb.w / stride;
-    int feat_h = lb.h / stride;
-    for (int h = 0; h < feat_h; h++) {
-        for (int w = 0; w < feat_w; w++) {
-            for (int a = 0; a < anchor_num; a++) {
-                int loc_idx = a * feat_w * feat_h * out_num + h * feat_w * out_num + w * out_num;
-                // process cls score
-                int class_index = 0;
-                float class_score = -FLT_MAX;
-                for (int s = 0; s <= cls_num - 1; s++) {
-                    float score = feat[loc_idx + 5 + s];
-                    if (score > class_score) {
-                        class_index = s;
-                        class_score = score;
-                    }
-                }
-                // process box score
-                float box_score = feat[loc_idx + 4];
-                float final_score = sigmoid(box_score) * sigmoid(class_score);
-                if (final_score >= prob_threshold) {
-                    float dx = sigmoid(feat[loc_idx + 0]);
-                    float dy = sigmoid(feat[loc_idx + 1]);
-                    float dw = sigmoid(feat[loc_idx + 2]);
-                    float dh = sigmoid(feat[loc_idx + 3]);
-                    float pred_cx = (dx * 2.0f - 0.5f + w) * stride;
-                    float pred_cy = (dy * 2.0f - 0.5f + h) * stride;
-                    float anchor_w = anchors[(anchor_group - 1) * 6 + a * 2 + 0];
-                    float anchor_h = anchors[(anchor_group - 1) * 6 + a * 2 + 1];
-                    float pred_w = dw * dw * 4.0f * anchor_w;
-                    float pred_h = dh * dh * 4.0f * anchor_h;
-                    float x0 = pred_cx - pred_w * 0.5f;
-                    float y0 = pred_cy - pred_h * 0.5f;
-                    float x1 = pred_cx + pred_w * 0.5f;
-                    float y1 = pred_cy + pred_h * 0.5f;
-
-                    Object obj;
-                    obj.rect.x = x0;
-                    obj.rect.y = y0;
-                    obj.rect.width = x1 - x0;
-                    obj.rect.height = y1 - y0;
-                    obj.label = class_index;
-                    obj.prob = final_score;
-                    /*if (0 == class_index)*/ {
-                        objects.push_back(obj);
-                        //log_debug("[%s] stride: %d, push back: %d\n", __FUNCTION__, stride, class_index);
-                    }
-                }
-            }
-        }
-    }
-}
-
 #ifdef USE_OPENCV
 
-static void draw_objects(const std::vector<Object>& objects, const cv::Mat& bgr, int cls) {
-    cv::Mat image = bgr.clone();
-
-    for (size_t i = 0; i < objects.size(); i++) {
-        const Object& obj = objects[i];
-
-        fprintf(stderr, "%2d: %3.0f%%, [%4.0f, %4.0f, %4.0f, %4.0f], %s\n", obj.label, obj.prob * 100, obj.rect.x,
-            obj.rect.y, obj.rect.x + obj.rect.width, obj.rect.y + obj.rect.height, class_names[obj.label]);
-
-        if (-1 != cls && obj.label != cls) continue;
-
-        cv::rectangle(image, obj.rect, cv::Scalar(255, 0, 0));
-
-        char text[256];
-        sprintf(text, "%s %.1f%%", class_names[obj.label], obj.prob * 100);
-
-        int baseLine = 0;
-        cv::Size label_size = cv::getTextSize(text, cv::FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseLine);
-
-        int x = obj.rect.x;
-        int y = obj.rect.y - label_size.height - baseLine;
-        if (y < 0)
-            y = 0;
-        if (x + label_size.width > image.cols)
-            x = image.cols - label_size.width;
-
-        cv::rectangle(image, cv::Rect(cv::Point(x, y), cv::Size(label_size.width, label_size.height + baseLine)),
-            cv::Scalar(255, 255, 255), -1);
-
-        cv::putText(image, text, cv::Point(x, y + label_size.height), cv::FONT_HERSHEY_SIMPLEX, 0.5,
-            cv::Scalar(0, 0, 0));
-    }
-
-    cv::imwrite("yolov5s_out.jpg", image);
-}
-
 // load input images
-static void get_input_data_focus(const char *image_file, uint8_t *input_data, float input_scale, int zero_point) {
+static void get_input_data_focus(const char *image_file, image &lb, float input_scale, int zero_point) {
     cv::Mat sample = cv::imread(image_file, 1);
     cv::Mat img;
 
@@ -214,7 +79,7 @@ static void get_input_data_focus(const char *image_file, uint8_t *input_data, fl
 
     // Generate a gray image for letterbox using opencv
     cv::Mat img_new(lb.w, lb.h, CV_32FC3,
-        cv::Scalar(0.5 / cov[1][0] + cov[0][0], 0.5 / cov[1][1] + cov[0][1], 0.5 / cov[1][2] + cov[0][2]));
+        cv::Scalar(0.5 / coco_image_cov[1][0] + coco_image_cov[0][0], 0.5 / coco_image_cov[1][1] + coco_image_cov[0][1], 0.5 / coco_image_cov[1][2] + coco_image_cov[0][2]));
     int top = (lb.h - resize_rows) / 2;
     int bot = (lb.h - resize_rows + 1) / 2;
     int left = (lb.w - resize_cols) / 2;
@@ -235,63 +100,14 @@ static void get_input_data_focus(const char *image_file, uint8_t *input_data, fl
             for (int c = 0; c < 3; c++) {
                 int in_index = h * lb.w * 3 + w * 3 + c;
                 int out_index = c * lb.w * lb.h + h * lb.w + w;
-                input_temp[out_index] = (img_data[in_index] - cov[0][c]) * cov[1][c];
+                input_temp[out_index] = (img_data[in_index] - coco_image_cov[0][c]) * coco_image_cov[1][c];
             }
         }
     }
 
     /* focus process: 3x640x640 -> 12x320x320 */
-    /*
-     | 0 2 |          C0-0, C1-0, C2-0,
-     | 1 3 | x C3 =>  C0-1, C1-1, C2-1, x C12
-                      C0-2, C1-2, C2-2,
-                      C0-3, C1-3, C2-3,
-    */
-    for (int i = 0; i < 2; i++) {       // corresponding to rows
-        for (int g = 0; g < 2; g++) {   // corresponding to cols
-            for (int c = 0; c < 3; c++) {
-                for (int h = 0; h < lb.h / 2; h++) {
-                    for (int w = 0; w < lb.w / 2; w++) {
-                        int in_index = i + g * lb.w + c * lb.w * lb.h +
-                            h * 2 * lb.w + w * 2;
-                        int out_index = i * 2 * 3 * (lb.w / 2) * (lb.h / 2) +
-                            g * 3 * (lb.w / 2) * (lb.h / 2) +
-                            c * (lb.w / 2) * (lb.h / 2) +
-                            h * (lb.w / 2) +
-                            w;
-
-                        /* quant to uint8 */
-                        int udata = (int)round(input_temp[in_index] / input_scale + (float)zero_point);
-                        if (255 < udata) udata = 255;
-                        else if (udata < 0) udata = 0;
-                        input_data[out_index] = udata;
-                    }
-                }
-            }
-        }
-    }
-
+    imi_utils_yolov5_focus_data(input_temp, lb, input_scale, zero_point);
     free(input_temp);
-}
-
-#else // !USE_OPENCV
-
-static int draw_objects(const std::vector<Object>& objects, image &img, int cls) {
-    size_t size = objects.size();
-    fprintf(stdout, "detected objects num: %zu\n", size);
-
-    for (size_t i = 0; i < size; i++) {
-        const Object& obj = objects[i];
-        fprintf(stdout, "[%2d]: %3.3f%%, [(%4.0f, %4.0f), (%4.0f, %4.0f)], %s\n",
-            obj.label, obj.prob * 100, obj.rect.x, obj.rect.y,
-            obj.rect.x + obj.rect.width, obj.rect.y + obj.rect.height, class_names[obj.label]);
-
-        if (-1 == cls || obj.label == cls) {
-            draw_box(img, obj.rect.x, obj.rect.y,
-                obj.rect.x + obj.rect.width, obj.rect.y + obj.rect.height, 2, 0, 255, 0);
-        }
-    }
-    return 0;
 }
 
 #endif // USE_OPENCV
@@ -300,9 +116,10 @@ static int draw_objects(const std::vector<Object>& objects, image &img, int cls)
 // 0: 1, 3, 20, 20, 85
 // 1: 1, 3, 40, 40, 85
 // 2: 1, 3, 80, 80, 85
-static int proposals_objects_get(graph_t &graph, tm_tensor_output_t pt, std::vector<Object> &proposals) {
+static int proposals_objects_get(graph_t &graph, tm_tensor_output_t pt,
+    std::vector<Object> &proposals, const Size2i &lb) {
     proposals.clear();
-    int stride = 32;
+    int stride = 32, anchor_group; // 32 -> 3, 16 -> 2, 8 -> 1
     for (int i = 2; -1 < i; i--) {
         uint8_t *p_data_u8 = (uint8_t *)pt[i].p_data;
         float *p_data = (float *)calloc(pt[i].p_count, sizeof(float));
@@ -310,62 +127,12 @@ static int proposals_objects_get(graph_t &graph, tm_tensor_output_t pt, std::vec
             p_data[c] = ((float)p_data_u8[c] - (float)pt[i].p_zero_point) * pt[i].p_scale;
         }
 
-        generate_proposals(stride, p_data, prob_threshold, proposals);
+        anchor_group = i + 1;
+        imi_utils_yolov5_proposals_generate(stride, lb, anchor_group,
+            p_data, prob_threshold, proposals, class_num);
         stride >>= 1;
-
-        free(p_data);
     }
     return 0;
-}
-
-static std::vector<Object> proposals_objects_filter(const std::vector<Object> &proposals, const Size2i &image_size) {
-    // apply nms with nms_threshold
-    std::vector<int> picked;
-    nms_sorted_bboxes(proposals, picked, nms_threshold);
-
-    int count = picked.size();
-    fprintf(stdout, "%d objects are picked\n", count);
-
-    // post process: scale and offset
-    float lb_scale;
-    if ((lb.h * 1.0 / image_size.height) < (lb.w * 1.0 / image_size.width)) {
-        lb_scale = lb.h * 1.0 / image_size.height;
-    }
-    else {
-        lb_scale = lb.w * 1.0 / image_size.width;
-    }
-    Size2i off = { int(lb_scale * image_size.width), int(lb_scale * image_size.height) };
-    off.width = (lb.w - off.width) / 2;
-    off.height = (lb.h - off.height) / 2;
-    log_debug("[%s] lb scale: %3.4f, off: (%d, %d)\n", __FUNCTION__, lb_scale, off.width, off.height);
-
-    std::vector<Object> objects(count);
-    for (int i = 0; i < count; i++) {
-        objects[i] = proposals[picked[i]];
-
-        float x0 = (objects[i].rect.x);
-        float y0 = (objects[i].rect.y);
-        float x1 = (objects[i].rect.x + objects[i].rect.width);
-        float y1 = (objects[i].rect.y + objects[i].rect.height);
-
-        // post process: from letter box to input image
-        x0 = (x0 - off.width) / lb_scale;
-        y0 = (y0 - off.height) / lb_scale;
-        x1 = (x1 - off.width) / lb_scale;
-        y1 = (y1 - off.height) / lb_scale;
-
-        x0 = std::max(std::min(x0, (float)(image_size.width - 1)), 0.f);
-        y0 = std::max(std::min(y0, (float)(image_size.height - 1)), 0.f);
-        x1 = std::max(std::min(x1, (float)(image_size.width - 1)), 0.f);
-        y1 = std::max(std::min(y1, (float)(image_size.height - 1)), 0.f);
-
-        objects[i].rect.x = x0;
-        objects[i].rect.y = y0;
-        objects[i].rect.width = x1 - x0;
-        objects[i].rect.height = y1 - y0;
-    }
-
-    return objects;
 }
 
 static void show_usage() {
@@ -392,6 +159,9 @@ int main(int argc, char* argv[]) {
     const char *model_file = nullptr;
     const char *image_file = nullptr;
     const char *output_file = "output.rgb";
+
+    // allow none square letterbox, set default letterbox size
+    image lb = make_image(640, 640, 3);
     image input = make_empty_image(640, 360, 3);
 
     int res, frame = 1, fc = 0;
@@ -499,8 +269,8 @@ int main(int argc, char* argv[]) {
     }
 
     /* get output tensor info */
-    tm_tensor_output_s tensor[3];
-    if (imi_utils_tm_get_graph_tensor(graph, tensor, 3, 1) < 0) {
+    tm_tensor_output_s tensor[NODE_CNT_YOLOV5S];
+    if (imi_utils_tm_get_graph_tensor(graph, tensor, NODE_CNT_YOLOV5S, 1) < 0) {
         fprintf(stderr, "[%s] get output tensor info failed\n", __FUNCTION__);
         return -1;
     }
@@ -538,7 +308,7 @@ int main(int argc, char* argv[]) {
 read_data:
     /* prepare process input data, set the data mem to input tensor */
 #ifdef USE_OPENCV
-    get_input_data_focus(image_file, (uint8_t *)(lb.data), input_scale, input_zero_point);
+    get_input_data_focus(image_file, lb, input_scale, input_zero_point);
 
 #ifdef CROSS_VALIDATION
     {
@@ -549,7 +319,7 @@ read_data:
 #endif // CROSS_VALIDATION
 
 #else // !USE_OPENCV
-    if (1 != (ret = get_input_data_yolov5_uint8(fp, lb, bgr, input, cov, input_scale, input_zero_point))) {
+    if (1 != (ret = imi_utils_yolov5_load_data(fp, input, bgr, lb, coco_image_cov, input_scale, input_zero_point))) {
         fprintf(stderr, "%s\n", ret ? "get_input_data error!" : "read input data fin");
         goto exit;
     }
@@ -571,21 +341,21 @@ read_data:
     }
 
     /* process the detection result */
-    if (proposals_objects_get(graph, tensor, proposals) < 0) {
+    if (proposals_objects_get(graph, tensor, proposals, Size2i(lb.w, lb.h)) < 0) {
         goto exit;
     }
 
     // sort all proposals by score from highest to lowest
-    qsort_descent_inplace(proposals);
+    imi_utils_objects_qsort(proposals);
     // filter objects
-    objects = proposals_objects_filter(proposals, Size2i(input.w, input.h));
+    objects = imi_utils_proposals_filter(proposals, Size2i(input.w, input.h), Size2i(lb.w, lb.h), nms_threshold);
 
     // draw objects
 #ifdef USE_OPENCV
-    draw_objects(objects, img, target_class);
+    imi_utils_objects_draw(objects, img, target_class, class_names, output_file);
 exit:
 #else // !USE_OPENCV
-    draw_objects(objects, input, target_class);
+    imi_utils_objects_draw(objects, input, target_class, class_names);
 
     // save result to output
     if (fout) {
